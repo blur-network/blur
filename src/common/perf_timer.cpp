3bec88abef0c46c4b9e17810960cbb0dc2dcc488
who-biz <37732338+who-biz@users.noreply.github.com>
2020-06-24 16:57:18 -0400

    Revert perf_timer.cpp/.h to original contents
    - reverts perf_timer files to state at repo initialization

--- src/common/perf_timer.cpp
+++ src/common/perf_timer.cpp
@@ -33,7 +33,7 @@
 #undef MONERO_DEFAULT_LOG_CATEGORY
 #define MONERO_DEFAULT_LOG_CATEGORY "perf"
 
-namespace tools
+namespace
 {
   uint64_t get_tick_count()
   {
@@ -83,7 +83,7 @@ namespace tools
 
 el::Level performance_timer_log_level = el::Level::Debug;
 
-static __thread std::vector<LoggingPerformanceTimer*> *performance_timers = NULL;
+static __thread std::vector<PerformanceTimer*> *performance_timers = NULL;
 
 void set_performance_timer_log_level(el::Level level)
 {
@@ -96,35 +96,36 @@ void set_performance_timer_log_level(el::Level level)
   performance_timer_log_level = level;
 }
 
-PerformanceTimer::PerformanceTimer(bool paused): started(true), paused(paused)
+PerformanceTimer::PerformanceTimer(const std::string &s, uint64_t unit, el::Level l): name(s), unit(unit), level(l), started(false), paused(false)
 {
-  if (paused)
-    ticks = 0;
-  else
-    ticks = get_tick_count();
-}
-
-LoggingPerformanceTimer::LoggingPerformanceTimer(const std::string &s, uint64_t unit, el::Level l): PerformanceTimer(), name(s), unit(unit), level(l)
-{
+  ticks = get_tick_count();
   if (!performance_timers)
   {
     MLOG(level, "PERF             ----------");
-    performance_timers = new std::vector<LoggingPerformanceTimer*>();
+    performance_timers = new std::vector<PerformanceTimer*>();
   }
   else
   {
+    PerformanceTimer *pt = performance_timers->back();
+    if (!pt->started && !pt->paused)
+    {
+      size_t size = 0; for (const auto *tmp: *performance_timers) if (!tmp->paused) ++size;
+      MLOG(pt->level, "PERF           " << std::string((size-1) * 2, ' ') << "  " << pt->name);
+      pt->started = true;
+    }
   }
   performance_timers->push_back(this);
 }
 
 PerformanceTimer::~PerformanceTimer()
 {
+  performance_timers->pop_back();
   if (!paused)
     ticks = get_tick_count() - ticks;
-}
-
-LoggingPerformanceTimer::~LoggingPerformanceTimer()
-{
+  char s[12];
+  snprintf(s, sizeof(s), "%8llu  ", (unsigned long long)(ticks_to_ns(ticks) / (1000000000 / unit)));
+  size_t size = 0; for (const auto *tmp: *performance_timers) if (!tmp->paused || tmp==this) ++size;
+  MLOG(level, "PERF " << s << std::string(size * 2, ' ') << "  " << name);
   if (performance_timers->empty())
   {
     delete performance_timers;
@@ -136,6 +137,7 @@ void PerformanceTimer::pause()
 {
   if (paused)
     return;
+  ticks = get_tick_count() - ticks;
   paused = true;
 }
 
@@ -143,6 +145,7 @@ void PerformanceTimer::resume()
 {
   if (!paused)
     return;
+  ticks = get_tick_count() - ticks;
   paused = false;
 }
 
