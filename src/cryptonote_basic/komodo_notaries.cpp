6575664b330193a51da7150b7f9673de1f0dfb7c
who-biz <37732338+who-biz@users.noreply.github.com>
2021-11-02 17:21:51 -0400

    Comment out not-yet-merged included file in komodo_notaries.cpp

--- src/cryptonote_basic/komodo_notaries.cpp
+++ src/cryptonote_basic/komodo_notaries.cpp
@@ -47,7 +47,7 @@
  ****************************************************************************************/
 
 #include "komodo_notaries.h"
-#include "notary_server/notary_server.h"
+//#include "notary_server/notary_server.h"
 #include "common/hex_str.h"
 #include "bitcoin/bitcoin.h"
 //#include "libbtc/include/btc/wallet.h"
