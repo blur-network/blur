e1bd783cdbc4498ce122d5864d1db4f03c8b1b63
who-biz <37732338+who-biz@users.noreply.github.com>
2021-11-30 15:40:05 -0500

    Revert improper NDEBUG movement in CMake
    - Fix unused var errors in build by wrapping assert cases in jh/skein hash_extra files

--- src/crypto/hash-extra-jh.c
+++ src/crypto/hash-extra-jh.c
@@ -37,6 +37,10 @@
 #include "hash-ops.h"
 
 void hash_extra_jh(const void *data, size_t length, char *hash) {
+#if !defined(NDEBUG)
   int r = jh_hash(HASH_SIZE * 8, data, 8 * length, (uint8_t*)hash);
   assert(SUCCESS == r);
+#else
+  jh_hash(HASH_SIZE * 8, data, 8 * length, (uint8_t*)hash);
+#endif
 }
--- src/crypto/hash-extra-skein.c
+++ src/crypto/hash-extra-skein.c
@@ -35,6 +35,10 @@
 #include "skein.h"
 
 void hash_extra_skein(const void *data, size_t length, char *hash) {
+#if !defined(NDEBUG)
   int r = skein_hash(8 * HASH_SIZE, data, 8 * length, (uint8_t*)hash);
   assert(SKEIN_SUCCESS == r);
+#else
+  skein_hash(8 * HASH_SIZE, data, 8 * length, (uint8_t*)hash);
+#endif
 }
