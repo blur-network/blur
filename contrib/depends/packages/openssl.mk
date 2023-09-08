ec025d84aec9a22147adca7cb7f9b7719c5ed958
who-biz <37732338+who-biz@users.noreply.github.com>
2023-09-08 19:44:58 +0000

    Update openssl package in depends for 3.0.2
    - 1.x.x versions are old at this point

--- contrib/depends/packages/openssl.mk
+++ contrib/depends/packages/openssl.mk
@@ -1,46 +1,67 @@
 package=openssl
-$(package)_version=1.1.1l
-$(package)_download_path=https://www.openssl.org/source
+$(package)_version=3.0.2
+$(package)_download_path=https://www.openssl.org/source/old/3.0/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1
-$(package)_patches=fix_darwin.patch
+$(package)_sha256_hash=98e91ccead4d4756ae3c9cde5e09191a8e586d9f4d50838e7ec09d6411dfdb63
 
 define $(package)_set_vars
-$(package)_config_env=AR="$($(package)_ar)" ARFLAGS=$($(package)_arflags) RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
-$(package)_config_env_android=ANDROID_NDK_HOME="$(host_prefix)/native" PATH="$(host_prefix)/native/bin" CC=clang AR=ar RANLIB=ranlib
-$(package)_build_env_android=ANDROID_NDK_HOME="$(host_prefix)/native"
+$(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
 $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
 $(package)_config_opts+=no-capieng
 $(package)_config_opts+=no-dso
 $(package)_config_opts+=no-dtls1
 $(package)_config_opts+=no-ec_nistp_64_gcc_128
+$(package)_config_opts+=no-aria
+$(package)_config_opts+=no-bf
+$(package)_config_opts+=no-blake2
+$(package)_config_opts+=no-camellia
+$(package)_config_opts+=no-cast
+$(package)_config_opts+=no-chacha
+$(package)_config_opts+=no-des
+$(package)_config_opts+=no-dh
+$(package)_config_opts+=no-dsa
+$(package)_config_opts+=no-ecdsa
+$(package)_config_opts+=no-ecdh
+$(package)_config_opts+=no-idea
+$(package)_config_opts+=no-md4
+$(package)_config_opts+=no-mdc2
+$(package)_config_opts+=no-ocb
+$(package)_config_opts+=no-poly1305
+$(package)_config_opts+=no-rc2
+$(package)_config_opts+=no-rc4
+$(package)_config_opts+=no-rmd160
+$(package)_config_opts+=no-scrypt
+$(package)_config_opts+=no-siphash
+$(package)_config_opts+=no-siv
+$(package)_config_opts+=no-sm2
+$(package)_config_opts+=no-sm3
+$(package)_config_opts+=no-sm4
+$(package)_config_opts+=no-whirlpool
 $(package)_config_opts+=no-gost
-$(package)_config_opts+=no-heartbeats
+$(package)_config_opts+=no-static-engine
+$(package)_config_opts+=no-uplink
 $(package)_config_opts+=no-md2
 $(package)_config_opts+=no-rc5
 $(package)_config_opts+=no-rdrand
 $(package)_config_opts+=no-rfc3779
 $(package)_config_opts+=no-sctp
+$(package)_config_opts+=no-stdio
 $(package)_config_opts+=no-shared
 $(package)_config_opts+=no-ssl-trace
-$(package)_config_opts+=no-ssl2
-$(package)_config_opts+=no-ssl3
 $(package)_config_opts+=no-unit-test
+$(package)_config_opts+=no-tests
 $(package)_config_opts+=no-weak-ssl-ciphers
 $(package)_config_opts+=no-zlib
 $(package)_config_opts+=no-asm
+$(package)_config_opts+=no-threads
 $(package)_config_opts+=no-egd
-$(package)_config_opts+=no-hw
 $(package)_config_opts+=no-zlib-dynamic
 $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
 $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
-$(package)_config_opts_freebsd=-fPIC -Wa,--noexecstack
 $(package)_config_opts_x86_64_linux=linux-x86_64
 $(package)_config_opts_i686_linux=linux-generic32
 $(package)_config_opts_arm_linux=linux-generic32
 $(package)_config_opts_aarch64_linux=linux-generic64
-$(package)_config_opts_aarch64_darwin=darwin64-arm64-cc
-$(package)_config_opts_riscv64_linux=linux-generic64
 $(package)_config_opts_mipsel_linux=linux-generic32
 $(package)_config_opts_mips_linux=linux-generic32
 $(package)_config_opts_powerpc_linux=linux-generic32
@@ -49,21 +70,16 @@ $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
 endef
 
-define $(package)_preprocess_cmds
-  sed -i.old 's|"engines", "apps", "test", "util", "tools", "fuzz"|"engines", "tools"|' Configure && \
-  patch -p1 < $($(package)_patch_dir)/fix_darwin.patch
-endef
-
 define $(package)_config_cmds
   ./Configure $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
-  $(MAKE) build_libs
+  $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install_sw
+  $(MAKE) INSTALL_PREFIX=$($(package)_staging_dir) -j1 install_sw
 endef
 
 define $(package)_postprocess_cmds
